d7d202c486f24761ea5a6e075b4851f5542b4d88
jiwon <zest1923@gmail.com>
2022-01-01 02:38:11 +0900

    변수와 상수

---
+++ 02.변수와 상수/variableAndLet/variableAndLet/main.swift
@@ -0,0 +1,10 @@
+let name: String = "jiwon"
+var age:Int = 24
+var job:String = "student"
+
+age = 25
+job = "programmer"
+
+print("안녕하세요, 제 이름은 \(name)입니다.")
+print("제 나이는 \(age) 입니다.")
+print("저는 \(job)가 되고 싶습니다.")
